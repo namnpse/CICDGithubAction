5e4aff8829fd9abd19d20e573b4f7714bf8b05cf
Nam Nguyen <namnpse@gmail.com>
2023-04-17 21:37:11 +0700

    add unit test

---
+++ app/src/test/java/com/namnp/cicdgithubaction/AdditionUnitTest.kt
@@ -0,0 +1,17 @@
+package com.namnp.cicdgithubaction
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class AdditionUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(10, 5 + 5)
+    }
+}
